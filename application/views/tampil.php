af96397084e59014e1cb8ae919083976cb48f1f8
ricochandra11 <richandraardia11@gmail.com>
2022-09-23 16:12:53 +0700

    Latihan-ci

---
+++ application/views/tampil.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+   <title>Latihan Pertamaa</title> 
+</head>
+<body>
+    <p>Latihan Pertama saya belajar framework codelgniter</p>
+    <p>Framework Codelgniter sangat mudah dipelajari</p>
+</body>
+</html>
